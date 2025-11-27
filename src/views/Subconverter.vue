a09d5064a91ef97cb379ba7132272e1f263174bb
科技-刘 <165335155+zxlwq@users.noreply.github.com>
2025-11-28 07:24:34 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -788,7 +788,7 @@ export default {
       form: {
         sourceSubUrl: "",
         clientType: "",
-        customBackend: this.getUrlParam() == "" ? "https://url.v1.mk" : this.getUrlParam(),
+        customBackend: this.getUrlParam() == "" ? "https://conver.zxlwq.dpdns.org" : this.getUrlParam(),
         shortType: "https://v1.mk/short",
         remoteConfig: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online.ini",
         excludeRemarks: "",
@@ -1312,5 +1312,6 @@ export default {
   }
 };
 </script>
+
 
 
